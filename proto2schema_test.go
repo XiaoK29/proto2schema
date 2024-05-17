5bac01322d3e35724705be185b37b049c617af89
xiaok29 <1526783667@qq.com>
2024-05-17 17:51:05 +0800

    🧪 test: 添加测试用例

--- proto2schema_test.go
+++ proto2schema_test.go
@@ -1,14 +1,15 @@
 package proto2schema
 
 import (
-	"fmt"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestProto2schema(t *testing.T) {
 	str := Proto2schema("./test.proto")
 	b, _ := os.ReadFile("./test.k")
 
-	fmt.Println(string(b) == str)
+	assert.Equal(t, string(b), str)
 }
